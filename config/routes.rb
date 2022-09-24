ba5d115ccb3c45d522c7142b6eb2cc18170d2c11
kakru Aoyama <kakeru0520sou1@gmail.com>
2022-09-24 23:53:09 +0900

    3-8

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'hello/index' => 'hello#index'
   get 'hello/link' => 'hello#link'
   get 'tweets' => 'tweets#index'
+  get 'tweets/new' => 'tweets#new'
   root 'hello#index'
 end
