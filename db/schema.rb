31d39652a86a48ef77006a25ef272bb9cf738183
kakru Aoyama <kakeru0520sou1@gmail.com>
2022-09-25 17:02:31 +0900

    7-4

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_25_060450) do
+ActiveRecord::Schema.define(version: 2022_09_25_075222) do
 
   create_table "tweets", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.string "image"
   end
 
   create_table "users", force: :cascade do |t|
